5893c2f4a0eca0b73997ff4ece8969e31c2864e8
Andres Leal <pixele9@gmail.com>
2025-07-25 22:25:52 -0600

    chore: added types to its types file

--- src/types.d.ts
+++ src/types.d.ts
@@ -11,3 +11,5 @@ export type TodoCompleted = Pick<Todo, 'completed'>
 export type TodoCreatedAt = Pick<Todo, 'createdAt'>
 
 export type ListOfTodos = Todo[];
+
+export type FilterValue = typeof TODO_FILTERS[keyof typeof TODO_FILTERS]
